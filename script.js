8d699c2ebc5b809b9c658a014fa82913defd7207
Nathaniel <nathanielmgagnon@gmail.com>
2024-01-28 17:44:14 -0500

    Added html and js files to repo

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstName = "Nathaniel ";
+var lastName = "Gagnon";
+var age = 17;
+
+alert("My name is " + firstName + lastName);
+alert("I am " + age + " years old")
+
+console.log("I am having a ton of fun in this class! The assignments are genuinely enjoyable and I am happy to be learning a good programming foundation!");
